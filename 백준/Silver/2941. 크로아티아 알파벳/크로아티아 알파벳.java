8d71c48f6aa22774d399fd7798212d1657fe2dd0
choijh0309 <128604591+choijh0309@users.noreply.github.com>
2024-05-13 20:52:07 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 204 ms, Memory: 17660 KB -BaekjoonHub

--- 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
@@ -13,10 +13,9 @@ public class Main {
  
 			char ch = str.charAt(i);
  
-			if(ch == 'c') {			// 만약 ch 가 c 라면?
+			if(ch == 'c') {			
 				if(i < str.length() - 1) {
-					if(str.charAt(i + 1) == '=') {		//만약 ch 다음 문자가 '=' 이라면?
-						// i+1 까지가 하나의 문자이므로 다음 문자를 건너 뛰기 위해 1 증가
+					if(str.charAt(i + 1) == '=') {
 						i++;		
 					}
 					else if(str.charAt(i + 1) == '-') {
@@ -29,13 +28,13 @@ public class Main {
 				if(i < str.length() - 1) {
 					if(str.charAt(i + 1) == 'z') {
 						if(i < str.length() - 2) {
-							if(str.charAt(i + 2) == '=') {	// dz= 일 경우
+							if(str.charAt(i + 2) == '=') {	
 								i += 2;
 							}
 						}
 					}
 		        
-					else if(str.charAt(i + 1) == '-') {	// d- 일 경우
+					else if(str.charAt(i + 1) == '-') {	
 						i++;
 					}
 				}
@@ -43,7 +42,7 @@ public class Main {
 		    
 			else if(ch == 'l') {
 				if(i < str.length() - 1) {
-					if(str.charAt(i + 1) == 'j') {	// lj 일 경우
+					if(str.charAt(i + 1) == 'j') {	
 						i++;
 					}
 				}
@@ -51,7 +50,7 @@ public class Main {
 		    
 			else if(ch == 'n') {
 				if(i < str.length() - 1) {
-					if(str.charAt(i + 1) == 'j') {	// nj 일 경우
+					if(str.charAt(i + 1) == 'j') {
 						i++;
 					}
 				}
@@ -59,7 +58,7 @@ public class Main {
  
 			else if(ch == 's') {
 				if(i < str.length() - 1) {
-					if(str.charAt(i + 1) == '=') {	// s= 일 경우
+					if(str.charAt(i + 1) == '=') {
 						i++;
 					}
 				}
@@ -67,7 +66,7 @@ public class Main {
  
 			else if(ch == 'z') {
 				if(i < str.length() - 1) {
-					if(str.charAt(i + 1) == '=') {	// z= 일 경우
+					if(str.charAt(i + 1) == '=') {	
 						i++;
 					}
 				}
