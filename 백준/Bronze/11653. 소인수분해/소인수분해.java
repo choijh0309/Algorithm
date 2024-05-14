4e632b01de9eb347e9e4bc01017f6ee6387a2fc7
choijh0309 <128604591+choijh0309@users.noreply.github.com>
2024-05-14 20:40:42 +0900

    [Bronze I] Title: 소인수분해, Time: 208 ms, Memory: 17736 KB -BaekjoonHub

---
+++ 백준/Bronze/11653. 소인수분해/소인수분해.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        StringBuilder sb = new StringBuilder();
+        
+        for (int i = 2; i * i <= N; i++) {
+            while (N % i == 0) {
+                sb.append(i).append('\n');
+                N /= i;
+            }
+        }
+        
+        if (N != 1) {
+            sb.append(N);
+        }
+        System.out.println(sb);
+    }
+}
