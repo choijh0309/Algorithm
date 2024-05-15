21baf27228c1397b668747501b2384da8061e17c
choijh0309 <128604591+choijh0309@users.noreply.github.com>
2024-05-15 13:23:26 +0900

    [Silver III] Title: 1로 만들기, Time: 124 ms, Memory: 14392 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,18 +1,17 @@
-import java.util.*;
+import java.io.*;
 
 public class Main {
-    static int N;
-    static int D[];
-    public static void main(String[] args) throws Exception {
-        Scanner sc = new Scanner(System.in);
-        N = sc.nextInt();
-        D = new int[N + 1];
-        D[1] = 0;
-        for (int i = 2; i <= N; i++) {
-            D[i] = D[i - 1] + 1;
-            if (i % 2 == 0)D[i] = Math.min(D[i], D[i / 2] + 1);
-            if (i % 3 == 0)D[i] = Math.min(D[i], D[i / 3] + 1);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int N = Integer.parseInt(br.readLine());
+        System.out.println(recur(N, 0));
+    }
+    
+    static int recur(int N, int count) {
+        if (N < 2) {
+            return count;
         }
-        System.out.println(D[N]);
+        return Math.min(recur(N / 2, count + 1 + (N % 2)), recur(N / 3, count + 1 + (N % 3)));
     }
 }
