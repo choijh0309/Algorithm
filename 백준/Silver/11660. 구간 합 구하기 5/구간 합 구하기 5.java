1ef9a0f356c1b2045ceddd6c6b887c398cb35150
choijh0309 <128604591+choijh0309@users.noreply.github.com>
2024-06-11 08:39:00 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 1376 ms, Memory: 128872 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -1,14 +1,14 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
         int A[][] = new int[N + 1][N + 1];
-        for (int i = 1; i <= N; i++) {
+        for (int i = 1; i <= N; i ++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 1; j <= N; j++) {
                 A[i][j] = Integer.parseInt(st.nextToken());
@@ -17,7 +17,7 @@ public class Main {
         int D[][] = new int[N + 1][N + 1];
         for (int i = 1; i <= N; i++) {
             for (int j = 1; j <= N; j++) {
-                D[i][j] = D[i][j - 1] + D[i - 1][j] - D[i - 1][j - 1] + A[i][j];
+                D[i][j] = D[i] [j - 1] + D[i - 1][j] - D[i - 1][j - 1] + A[i][j];
             }
         }
         for (int i = 0; i < M; i++) {
