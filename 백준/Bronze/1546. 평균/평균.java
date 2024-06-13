fc34f6fdc348fc371f6c736d5ac4c4d421483a2d
choijh0309 <128604591+choijh0309@users.noreply.github.com>
2024-06-14 08:09:51 +0900

    [Bronze I] Title: 평균, Time: 240 ms, Memory: 21028 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.java
+++ 백준/Bronze/1546. 평균/평균.java
@@ -10,10 +10,12 @@ public class Main {
         }
         long sum = 0;
         long max = 0;
+        
         for (int i = 0; i < N; i++) {
             if (A[i] > max) max = A[i];
             sum = sum + A[i];
         }
+        
         System.out.println(sum * 100.0 / max / N);
     }
 }
