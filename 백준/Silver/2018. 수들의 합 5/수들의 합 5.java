542aa312ba01ef4abbf646b53220a26889a092f4
choijh0309 <128604591+choijh0309@users.noreply.github.com>
2024-06-17 07:49:10 +0900

    [Silver V] Title: 수들의 합 5, Time: 248 ms, Memory: 17872 KB -BaekjoonHub

--- 백준/Silver/2018. 수들의 합 5/수들의 합 5.java
+++ 백준/Silver/2018. 수들의 합 5/수들의 합 5.java
@@ -5,14 +5,12 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
         int count = 1;
-        int start_index = 1; 
+        int start_index = 1;
         int end_index = 1;
         int sum = 1;
         while (end_index != N) {
             if (sum == N) {
-                count++;
-                end_index++;
-                sum = sum + end_index;
+                count++; end_index++; sum = sum + end_index;
             } else if (sum > N) {
                 sum = sum - start_index;
                 start_index++;
