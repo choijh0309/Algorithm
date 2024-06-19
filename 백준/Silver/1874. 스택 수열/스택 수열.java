ebc14e374f876e3ec3d5519d500bc8b6e0133c2c
choijh0309 <128604591+choijh0309@users.noreply.github.com>
2024-06-20 06:37:53 +0900

    [Silver II] Title: 스택 수열, Time: 1004 ms, Memory: 112156 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.java
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -1,18 +1,17 @@
-import java.util.Scanner;
-import java.util.Stack;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
-        int[] A = new int[N];
+        int A[] = new int[N];
         for (int i = 0; i < N; i++) {
             A[i] = sc.nextInt();
         }
         Stack<Integer> stack = new Stack<>();
-        StringBuffer bf = new StringBuffer();
-        int num = 1; 
+        int num = 1;
         boolean result = true;
+        StringBuffer bf = new StringBuffer();
         for (int i = 0; i < A.length; i++) {
             int su = A[i];
             if (su >= num) {
