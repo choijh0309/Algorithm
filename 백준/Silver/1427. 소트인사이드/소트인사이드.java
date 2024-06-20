3ea029fcd3a96a11351c38976aee5e172d59e108
choijh0309 <128604591+choijh0309@users.noreply.github.com>
2024-06-21 06:49:59 +0900

    [Silver V] Title: 소트인사이드, Time: 168 ms, Memory: 17632 KB -BaekjoonHub

--- 백준/Silver/1427. 소트인사이드/소트인사이드.java
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.java
@@ -1,18 +1,20 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String str = sc.next();
-        int[] A = new int[str.length()];
+        int A[] = new int[str.length()];
         for (int i = 0; i < str.length(); i++) {
             A[i] = Integer.parseInt(str.substring(i, i + 1));
         }
+        // 선택정렬
         for (int i = 0; i < str.length(); i++) {
             int Max = i;
             for (int j = i + 1; j < str.length(); j++) {
-                if (A[j] > A[Max]) 
+                if (A[j] > A[Max]) {
                     Max = j;
+                }
             }
             if (A[i] < A[Max]) {
                 int temp = A[i];
@@ -23,5 +25,5 @@ public class Main {
         for (int i = 0; i < str.length(); i++) {
             System.out.print(A[i]);
         }
-    }
+    } 
 }
