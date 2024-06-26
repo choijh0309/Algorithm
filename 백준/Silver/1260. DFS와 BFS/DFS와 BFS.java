a232a11204c43665a8c13cdd9af7dfeecc8f364a
choijh0309 <128604591+choijh0309@users.noreply.github.com>
2024-06-26 16:12:48 +0900

    [Silver II] Title: DFS와 BFS, Time: 376 ms, Memory: 21556 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -1,33 +1,44 @@
+import java.io.*;
 import java.util.*;
 
 public class Main {
     static boolean visited[];
     static ArrayList<Integer>[] A;
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int N = scan.nextInt();
-        int M = scan.nextInt();
-        int Start = scan.nextInt();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int Start = Integer.parseInt(st.nextToken());
+        
         A = new ArrayList[N + 1];
         for (int i = 1; i <= N; i++) {
             A[i] = new ArrayList<Integer>();
         }
+        
         for (int i = 0; i < M; i++) {
-            int S = scan.nextInt();
-            int E = scan.nextInt();
+            st = new StringTokenizer(br.readLine());
+            int S = Integer.parseInt(st.nextToken());
+            int E = Integer.parseInt(st.nextToken());
             A[S].add(E);
             A[E].add(S);
         }
+        
         for (int i = 1; i <= N; i++) {
             Collections.sort(A[i]);
         }
+        
         visited = new boolean[N + 1];
         DFS(Start);
         System.out.println();
+        
         visited = new boolean[N + 1];
         BFS(Start);
         System.out.println();
     }
+
     public static void DFS(int Node) {
         System.out.print(Node + " ");
         visited[Node] = true;
@@ -37,6 +48,7 @@ public class Main {
             }
         }
     }
+
     private static void BFS(int Node) {
         Queue<Integer> queue = new LinkedList<Integer>();
         queue.add(Node);
