2d0ab6a1f2b9847c2e06d5ae8b73702ec2ad3e73
choijh0309 <128604591+choijh0309@users.noreply.github.com>
2024-06-27 11:18:13 +0900

    [Silver I] Title: 단지번호붙이기, Time: 116 ms, Memory: 14160 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -1,61 +1,86 @@
+
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
 
-	static int N, houseCount;
-	static char[][] graph;
-	static boolean[][] visited;
-	static int[] dy = {-1, 1, 0, 0};
-	static int[] dx = {0, 0, -1, 1};
-	static ArrayList<Integer> complexSizes = new ArrayList<>();
+    static int n;
+    static char[][] graph;
+    static boolean[][] visited;
+    static int[] dy = {-1, 1, 0, 0};
+    static int[] dx = {0, 0, -1, 1};
+    static ArrayList<Integer> complexSizes = new ArrayList<>();
 
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
+    static class Node {
+        int y, x;
+        Node(int y, int x) {
+            this.y = y;
+            this.x = x;
+        }
+    }
 
-		N = Integer.parseInt(st.nextToken());
+    static Queue<Node> queue = new ArrayDeque<>();
 
-		graph = new char[N][];
-		visited = new boolean[N][N];
+    static int bfs(int y, int x) {
+        int houseCount = 0;
+        // 해당 노드 방문 처리
+        visited[y][x] = true;
+        queue.offer(new Node(y, x));
 
-		for (int i = 0; i < N; i++) {
-			graph[i] = br.readLine().toCharArray();
-		}
+        while (!queue.isEmpty()) {
+            Node node = queue.poll();
+            houseCount++;
+            for (int d = 0; d < 4; d++) {
+                int ny = node.y + dy[d];
+                int nx = node.x + dx[d];
 
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < N; j++) {
-				if (graph[i][j] == '1' && !visited[i][j]) {
-					houseCount = 0;
-					dfs(i, j);
-					complexSizes.add(houseCount);
-				}
-			}
-		}
+                if (ny < 0 || nx < 0 || ny >= n || nx >= n || visited[ny][nx] || graph[ny][nx] != '1') continue;
+                visited[ny][nx] = true;
+                queue.offer(new Node(ny, nx));
+            }
+        }
+        return houseCount;
+    }
 
-		Collections.sort(complexSizes);
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-		System.out.println(complexSizes.size());
-		for (int size : complexSizes) {
-			System.out.println(size);
-		}
-	}
+        // N을 공백을 기준으로 구분하여 입력 받기
+        n = Integer.parseInt(st.nextToken());
 
-	static void dfs(int y, int x) {
-		visited[y][x] = true;
-		houseCount++;
+        graph = new char[n][];
+        visited = new boolean[n][n];
 
-		for (int d = 0; d < 4; d++) {
-			int ny = y + dy[d];
-			int nx = x + dx[d];
+        // 2차원 리스트의 맵 정보 입력 받기
+        for (int i = 0; i < n; i++) {
+            graph[i] = br.readLine().toCharArray();
+        }
 
-			if (ny < 0 || nx < 0 || ny >= N || nx >= N || visited[ny][nx] || graph[ny][nx] != '1') continue;
-			dfs(ny, nx);
-		}
-	}
+        // 모든 노드(위치)에 대하여 단지 수 찾기
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                // 현재 위치에서 BFS 수행
+                if (graph[i][j] == '1' && !visited[i][j]) {
+                    int size = bfs(i, j);
+                    complexSizes.add(size);
+                }
+            }
+        }
+
+        Collections.sort(complexSizes);
+
+        System.out.println(complexSizes.size());
+        for (int size : complexSizes) {
+            System.out.println(size);
+        }
+    }
 }
 
 /*
