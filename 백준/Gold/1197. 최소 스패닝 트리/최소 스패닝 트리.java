382af9d784dcef460ac727e2375344e848b354f8
choijh0309 <128604591+choijh0309@users.noreply.github.com>
2024-07-02 15:47:42 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 500 ms, Memory: 54432 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -1,60 +1,69 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int[] parent;
-    static PriorityQueue<pEdge> queue;
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int M = sc.nextInt();
-        queue = new PriorityQueue<>();
-        parent = new int[N + 1];
-        for (int i = 0; i < N; i++) {
-            parent[i] = i;
+	
+	static int V, E, result;
+	static ArrayList<Vertex>[] adjList;
+	static boolean[] visit;
+	static PriorityQueue<Vertex> pqueue = new PriorityQueue<>((v1, v2) -> v1.c - v2.c);
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		V = Integer.parseInt(st.nextToken());
+		E = Integer.parseInt(st.nextToken());
+		
+		adjList = new ArrayList[V + 1];
+		visit = new boolean[V + 1];
+		
+		for (int i = 1; i <= V; i++) {
+            adjList[i] = new ArrayList<>();
         }
-        for (int i = 0; i < M; i++) {
-            int s = sc.nextInt();
-            int e = sc.nextInt();
-            int v = sc.nextInt();
-            queue.add(new pEdge(s, e, v));
-        } 
-        int useEdge = 0;
-        int result = 0;
-        while (useEdge < N - 1) {
-            pEdge now = queue.poll();
-            if (find(now.s) != find(now.e)) {
-                union(now.s, now.e);
-                result = result + now.v;
-                useEdge++;
+		
+		for (int i = 0; i < E; i++) {
+			st = new StringTokenizer(br.readLine());
+			int v1 = Integer.parseInt(st.nextToken());
+			int v2 = Integer.parseInt(st.nextToken());
+			int c = Integer.parseInt(st.nextToken());
+			
+			adjList[v1].add(new Vertex(v2, c));
+            adjList[v2].add(new Vertex(v1, c));
+		}
+		
+		visit[1] = true;
+		for (Vertex vertex : adjList[1]) {
+            pqueue.offer(vertex);
+        }
+		int cnt = 1;
+		
+		while (!pqueue.isEmpty()) {
+			Vertex vertex = pqueue.poll();
+			if (visit[vertex.v]) continue;
+			visit[vertex.v] = true;
+			result += vertex.c;
+			cnt++;
+			if (cnt == V) break;
+			
+			for (Vertex nextVertex : adjList[vertex.v]) {
+                if (!visit[nextVertex.v]) {
+                    pqueue.offer(nextVertex);
+                }
             }
-        }
-        System.out.println(result);
-    }
-    public static void union(int a, int b) {
-        a = find(a);
-        b = find(b);
-        if (a != b) {
-            parent[b] = a;
-        }
-    }
-    public static int find(int a) {
-        if (a == parent[a]) 
-            return a;
-        else
-            return parent[a] = find(parent[a]);
-    }
-}
-class pEdge implements Comparable<pEdge> {
-    int s;
-    int e;
-    int v;
-    pEdge(int s, int e, int v) {
-        this.s = s;
-        this.e = e;
-        this.v = v;
-    }
-    @Override
-    public int compareTo(pEdge o) {
-        return this.v - o.v;
-    }
+		}
+		System.out.println(result);
+		
+	}
+	
+	static class Vertex {
+		int v, c;
+		Vertex(int v, int c) {
+			this.v = v;
+			this.c = c;
+		}
+	}
+	
 }
