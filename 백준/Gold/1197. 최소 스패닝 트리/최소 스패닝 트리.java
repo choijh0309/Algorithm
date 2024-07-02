9e0774855b0088c80f79cf211f0433b1089c0899
choijh0309 <128604591+choijh0309@users.noreply.github.com>
2024-07-02 15:50:25 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 468 ms, Memory: 54280 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -1,3 +1,4 @@
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
@@ -5,65 +6,64 @@ import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
-	
-	static int V, E, result;
-	static ArrayList<Vertex>[] adjList;
-	static boolean[] visit;
-	static PriorityQueue<Vertex> pqueue = new PriorityQueue<>((v1, v2) -> v1.c - v2.c);
-	
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		V = Integer.parseInt(st.nextToken());
-		E = Integer.parseInt(st.nextToken());
-		
-		adjList = new ArrayList[V + 1];
-		visit = new boolean[V + 1];
-		
-		for (int i = 1; i <= V; i++) {
+    
+    static int V, E, result;
+    static ArrayList<Edge>[] adjList;
+    static boolean[] visit;
+    static PriorityQueue<Edge> pqueue = new PriorityQueue<>((v1, v2) -> v1.c - v2.c);
+    
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        V = Integer.parseInt(st.nextToken());
+        E = Integer.parseInt(st.nextToken());
+        
+        adjList = new ArrayList[V + 1];
+        visit = new boolean[V + 1];
+        
+        for (int i = 1; i <= V; i++) {
             adjList[i] = new ArrayList<>();
         }
-		
-		for (int i = 0; i < E; i++) {
-			st = new StringTokenizer(br.readLine());
-			int v1 = Integer.parseInt(st.nextToken());
-			int v2 = Integer.parseInt(st.nextToken());
-			int c = Integer.parseInt(st.nextToken());
-			
-			adjList[v1].add(new Vertex(v2, c));
-            adjList[v2].add(new Vertex(v1, c));
-		}
-		
-		visit[1] = true;
-		for (Vertex vertex : adjList[1]) {
-            pqueue.offer(vertex);
+        
+        for (int i = 0; i < E; i++) {
+            st = new StringTokenizer(br.readLine());
+            int v1 = Integer.parseInt(st.nextToken());
+            int v2 = Integer.parseInt(st.nextToken());
+            int weight = Integer.parseInt(st.nextToken());
+            
+            adjList[v1].add(new Edge(v2, weight));
+            adjList[v2].add(new Edge(v1, weight));
         }
-		int cnt = 1;
-		
-		while (!pqueue.isEmpty()) {
-			Vertex vertex = pqueue.poll();
-			if (visit[vertex.v]) continue;
-			visit[vertex.v] = true;
-			result += vertex.c;
-			cnt++;
-			if (cnt == V) break;
-			
-			for (Vertex nextVertex : adjList[vertex.v]) {
-                if (!visit[nextVertex.v]) {
-                    pqueue.offer(nextVertex);
+        
+        visit[1] = true;
+        for (Edge edge : adjList[1]) {
+            pqueue.offer(edge);
+        }
+        
+        int cnt = 1;
+        
+        while (!pqueue.isEmpty()) {
+            Edge edge = pqueue.poll();
+            if (visit[edge.v]) continue;
+            visit[edge.v] = true;
+            result += edge.c;
+            cnt++;
+            if (cnt == V) break;
+            
+            for (Edge nextEdge : adjList[edge.v]) {
+                if (!visit[nextEdge.v]) {
+                    pqueue.offer(nextEdge);
                 }
             }
-		}
-		System.out.println(result);
-		
-	}
-	
-	static class Vertex {
-		int v, c;
-		Vertex(int v, int c) {
-			this.v = v;
-			this.c = c;
-		}
-	}
-	
+        }
+        System.out.println(result);
+    }
+    
+    static class Edge {
+        int v, c;
+        Edge(int v, int c) {
+            this.v = v;
+            this.c = c;
+        }
+    }
 }
