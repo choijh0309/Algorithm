816abe3ad01d678d0987dc35cfe7535c25a1979c
choijh0309 <128604591+choijh0309@users.noreply.github.com>
2024-07-03 14:48:03 +0900

    [Gold V] Title: 하노이 탑 이동 순서, Time: 424 ms, Memory: 36908 KB -BaekjoonHub

--- 백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.java
+++ 백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.java
@@ -1,29 +1,37 @@
-import java.io.*;
 
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+// hanoi(n) = 2 x hanoi(n - 1) + 1
+// hanoi(n - 1) = 2 x hanoi(n - 2) + 1
 public class Main {
-    public static StringBuilder sb = new StringBuilder();
-    
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        int N = Integer.parseInt(br.readLine());
-        
-        sb.append((int) (Math.pow(2, N) - 1)).append('\n');
-        
-        Hanoi(N, 1, 2, 3);
-        
-        System.out.print(sb);
-    }
-    
-    public static void Hanoi(int N, int start, int mid, int to) {
-        if (N == 1) {
-            sb.append(start + " " + to + "\n");
-            return;
-        }
-        Hanoi(N - 1, start, to, mid);
-        
-        sb.append(start + " " + to + "\n");
-        
-        Hanoi(N - 1, mid, start, to);
-    }
+
+	static int N;
+	static StringBuilder sb = new StringBuilder();
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+
+		sb.append(((int)Math.pow(2, N)) - 1).append("\n"); // 하노이 수행수
+		hanoi(N, 1, 2, 3);
+		System.out.println(sb);
+	}
+
+	// from(1번째 파라미터) 자리에서 to (3번째 파라미터) 로 n 개 이동하려고 할 때,
+	// (1) from 자의 n - 1 개를 temp 로 옮기고
+	// (2) from 자리에 남은 1개(n) 를 to 로 옮기고 (출력으로 표현)
+	// (3) temp 로 옮겼던 n - 1 개를 to 로 옮긴다.
+	// 옮기는 작업은 첫 번째 파라미터 자리에서 세 번째 파라미터 자리
+	static void hanoi(int n, int from, int temp, int to) {
+		// 기저 조건
+		if (n == 0) return;
+		
+		// (1)
+		hanoi(n - 1, from, to, temp);
+		// (2)
+		sb.append(from).append(" ").append(to).append("\n");
+		// (3)
+		hanoi(n - 1, temp, from, to);
+	}
 }
