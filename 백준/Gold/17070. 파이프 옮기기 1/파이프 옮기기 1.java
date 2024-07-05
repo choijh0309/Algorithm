77308cfd5a6b3edffd4e15f928f01b3ac19f3531
choijh0309 <128604591+choijh0309@users.noreply.github.com>
2024-07-05 15:46:28 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 108 ms, Memory: 14436 KB -BaekjoonHub

--- 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
@@ -3,61 +3,44 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+// dp
 public class Main {
 
 	static int N;
 	static int[][] map;
-	static int ans;
+	static long[][][] dp;
+	static long cnt;
 	
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(br.readLine());
 		
-		map = new int[N + 1][N + 1];
+		map = new int[N + 1][N + 1]; // 0 dummy
+		dp = new long[N + 1][N + 1][3];
 		for (int i = 1; i <= N; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			for (int j = 1; j <= N; j++) {
 				map[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
+		// 풀이 dp()
+		dp[1][2][1] = 1;
 		
-		ans = 0;
-		DFS(1, 2, 0);
+		for (int y = 1; y <= N; y++) {
+			for (int x = 3; x <= N; x++) {
+				if (map[y][x] == 0) {
+					dp[y][x][1] = dp[y][x - 1][1] + dp[y][x - 1][0];
+					dp[y][x][2] = dp[y - 1][x][2] + dp[y - 1][x][0];
+					if (map[y - 1][x] == 0 && map[y][x - 1] == 0) {
+						dp[y][x][0] = dp[y - 1][x - 1][0] + dp[y - 1][x - 1][1] + dp[y - 1][x - 1][2];
+					}
+				}
+			}
+		}
 		
-		System.out.println(ans);
-
+		long cnt = dp[N][N][0] + dp[N][N][1] + dp[N][N][2];
+		System.out.println(cnt);
 	}
 
-	static void DFS(int y, int x, int direction) {
-		if (y == N && x == N) {
-			ans++;
-			return;
-		}
-		
-		switch (direction) {
-		case 0:
-			if (x + 1 <= N && map[y][x + 1] == 0) {
-				DFS(y, x + 1, 0);
-			}
-			break;
-		case 1:
-			if (y + 1 <= N && map[y + 1][x] == 0) {
-				DFS(y + 1, x, 1);
-			}
-			break;
-		case 2:
-			if (x + 1 <= N && map[y][x + 1] == 0) {
-				DFS(y, x + 1, 0);
-			}
-			
-			if (y + 1 <= N && map[y + 1][x] == 0) {
-				DFS(y + 1, x, 1);
-			}
-			break;
-		}
-		
-		if (x + 1 <= N && y + 1 <= N && map[y][x + 1] == 0 && map[y + 1][x] == 0 && map[y + 1][x + 1] == 0) {
-			DFS(y + 1, x + 1, 2);
-		}
-	}
+	
 }
