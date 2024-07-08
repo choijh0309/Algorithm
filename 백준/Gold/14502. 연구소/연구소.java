a1a3f0f0e352826fcbfe5ee4f8d9c1ea9268d778
choijh0309 <128604591+choijh0309@users.noreply.github.com>
2024-07-08 09:29:01 +0900

    [Gold IV] Title: 연구소, Time: 304 ms, Memory: 26460 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.java
+++ 백준/Gold/14502. 연구소/연구소.java
@@ -1,25 +1,21 @@
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
 import java.util.ArrayList;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
-//조합 + bfs + Node
+// 조합 + dfs + int[]
 public class Main {
 
 	static int N, M, max;
 	static int[][] map, copyMap;
-	static ArrayList<Node> zero = new ArrayList<Node>();
-	static ArrayList<Node> virus = new ArrayList<Node>();
+	static ArrayList<int[]> zero = new ArrayList<>();
+	static ArrayList<int[]> virus = new ArrayList<>();
 	static int zeroSize;
-	static Node[] wall = new Node[3];
+	static int[][] wall = new int[3][2];
 	
 	static int[] dy = { 1, -1, 0, 0 };
 	static int[] dx = { 0, 0, -1, 1 };
-	
-	static Queue<Node> queue = new ArrayDeque<Node>();
 	
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -30,20 +26,20 @@ public class Main {
 		map = new int[N][M];
 		copyMap = new int[N][M];
 		
-		max = Integer.MIN_VALUE;
-		
 		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
 			for (int j = 0; j < M; j++) {
 				int n = Integer.parseInt(st.nextToken());
 				map[i][j] = n;				
 				if( n == 0 ) {
-					zero.add(new Node(i, j));
+					zero.add(new int[] {i,j});
 				}else if( n == 2) {
-					virus.add(new Node(i, j));
+					virus.add(new int[] {i,j});
 				}
 			}
 		}
+		
+		max = Integer.MIN_VALUE; // 이게 필요한가?????? 확인 후 이거 지워라
 		
 		zeroSize = zero.size();
 		comb( 0, 0 );
@@ -67,41 +63,26 @@ public class Main {
 	}
 	
 	static void check() {
-
 		//////// map copy 하면서 copyMap 자동으로 초기화
 		for (int i = 0; i < N; i++) {
 			for (int j = 0; j < M; j++) {
 				copyMap[i][j] = map[i][j];
 			}
 		}
-		
-		//////// wall
+
+		// wall
 		for (int i = 0; i < 3; i++) {
-			int y = wall[i].y;
-			int x = wall[i].x;
+			int y = wall[i][0];
+			int x = wall[i][1];
 			copyMap[y][x] = 1;
 		} 
 		
-		//////// virus
-		// bfs
-		// 초기 virus 를 queue 에 담는다.
-		queue.addAll(virus);
-
-		// bfs 로 virus 퍼지게
-		while( !queue.isEmpty() ) {
-			Node n = queue.poll();
-
-			for (int i = 0; i < 4; i++) {
-				int ny = n.y + dy[i];
-				int nx = n.x + dx[i];
-				if( ny < 0 || nx < 0 || ny >= N || nx >= M ) continue;
-				if( copyMap[ny][nx] == 0 ) { // 빈 칸이면
-					copyMap[ny][nx] = 2;
-					queue.offer(new Node( ny, nx ));
-				}
-			}
+		////////virus
+		// 초기 virus 각각 dfs() 수행
+		for (int[] v : virus) {			
+			dfs( v[0], v[1] );
 		}
-		
+
 		// virus 가 퍼지고 난 뒤 남은 0 계산
 		int sum = 0;
 		for (int i = 0; i < N; i++) {
@@ -113,11 +94,16 @@ public class Main {
 		// max 갱신
 		max = Math.max(max, sum);
 	}
-	
-	static class Node{
-		int y, x;
-		Node(int y, int x) {
-			this.y = y; this.x = x;
+
+	static void dfs( int y, int x ) {
+		for (int i = 0; i < 4; i++) {
+			int ny = y + dy[i];
+			int nx = x + dx[i];
+			if( ny < 0 || nx < 0 || ny >= N || nx >= M  ) continue;
+			if( copyMap[ny][nx] == 0 ) {
+				copyMap[ny][nx] = 2;
+				dfs( ny, nx );
+			}
 		}
 	}
 }
