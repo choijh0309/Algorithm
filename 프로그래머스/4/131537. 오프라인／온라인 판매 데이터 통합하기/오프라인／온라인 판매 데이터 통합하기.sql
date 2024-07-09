0d49fe95c714580dea208d138b84366a49928e6a
choijh0309 <128604591+choijh0309@users.noreply.github.com>
2024-07-09 22:16:26 +0900

    [level 4] Title: 오프라인/온라인 판매 데이터 통합하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/131537. 오프라인／온라인 판매 데이터 통합하기/오프라인／온라인 판매 데이터 통합하기.sql
+++ 프로그래머스/4/131537. 오프라인／온라인 판매 데이터 통합하기/오프라인／온라인 판매 데이터 통합하기.sql
@@ -3,7 +3,8 @@ SELECT date_format(sales_date, '%Y-%m-%d') as sales_date, product_id, user_id, s
   from online_sale
  where Month(sales_date) = 3
  union
+   all
 select date_format(sales_date, '%Y-%m-%d') as sales_date, product_id, null as user_id, sales_amount 
   from offline_sale
  where Month(sales_date) = 3
- order by sales_date, product_id, user_id; 
+ order by sales_date, product_id, user_id;
