fa2f5a52c2ed2186bd2b529f052009e18a5cd61a
choijh0309 <128604591+choijh0309@users.noreply.github.com>
2024-08-03 20:47:19 +0900

    [level 2] Title: 가장 큰 수, Time: 188.03 ms, Memory: 132 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -1,16 +1,34 @@
 import java.util.*;
-import java.util.stream.*;
 
 class Solution {
     public String solution(int[] numbers) {
-        return Arrays.stream(numbers)
-                    .mapToObj(String::valueOf)
-                    .sorted((s1, s2) -> {
-                        int original = Integer.parseInt(s1 + s2);
-                        int reversed = Integer.parseInt(s2 + s1);
-                        return reversed - original;
-                    })
-                    .collect(Collectors.joining(""))
-                    .replaceAll("^0+", "0");
+        // Convert int array to String array
+        String[] strNumbers = new String[numbers.length];
+        for (int i = 0; i < numbers.length; i++) {
+            strNumbers[i] = String.valueOf(numbers[i]);
+        }
+        
+        // Sort using custom comparator
+        Arrays.sort(strNumbers, new Comparator<String>() {
+            @Override
+            public int compare(String a, String b) {
+                String order1 = a + b;
+                String order2 = b + a;
+                return order2.compareTo(order1); // Reverse order
+            }
+        });
+        
+        // If, after sorting, the largest number is '0', the entire number is zero
+        if (strNumbers[0].equals("0")) {
+            return "0";
+        }
+        
+        // Build the largest number
+        StringBuilder largestNumber = new StringBuilder();
+        for (String num : strNumbers) {
+            largestNumber.append(num);
+        }
+        
+        return largestNumber.toString();
     }
 }
