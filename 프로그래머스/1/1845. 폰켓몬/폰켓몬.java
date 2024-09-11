6248afb0458fe958049ed9451fd2c0ef856a782c
choijh0309 <128604591+choijh0309@users.noreply.github.com>
2024-09-11 09:13:25 +0900

    [level 1] Title: 폰켓몬, Time: 2.80 ms, Memory: 80.5 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -3,16 +3,13 @@ import java.util.Set;
 
 class Solution {
     public int solution(int[] nums) {
-        // 선택할 수 있는 폰켓몬의 수
-        int selectableCount = nums.length / 2;
-        
-        // 중복을 제거하여 유니크한 폰켓몬의 종류를 구합니다
-        Set<Integer> uniquePokemons = new HashSet<>();
+        Set<Integer> uniqueMons = new HashSet<>();
         for (int num : nums) {
-            uniquePokemons.add(num);
+            uniqueMons.add(num);
         }
         
-        // 유니크한 폰켓몬의 종류 수와 선택 가능한 폰켓몬의 수 중 더 작은 값을 반환합니다
-        return Math.min(uniquePokemons.size(), selectableCount);
+        int maxSelectable = nums.length / 2;
+        
+        return Math.min(uniqueMons.size(), maxSelectable);
     }
 }
