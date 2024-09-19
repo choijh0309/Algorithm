5dd03bac592b77730703a9e1c5e62e47b74b8657
choijh0309 <128604591+choijh0309@users.noreply.github.com>
2024-09-19 16:00:36 +0900

    [level 2] Title: 의상, Time: 0.13 ms, Memory: 72.5 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -3,22 +3,23 @@ import java.util.Map;
 
 class Solution {
     public int solution(String[][] clothes) {
-        // 의상 종류별 개수를 저장할 Map
-        Map<String, Integer> clothesMap = new HashMap<>();
-        
-        // 의상 종류별 개수 계산
-        for (String[] cloth : clothes) {
-            String type = cloth[1];
-            clothesMap.put(type, clothesMap.getOrDefault(type, 0) + 1);
+        HashMap<String, Integer> map = new HashMap<>();
+
+        // 의상의 종류별로 개수 카운트
+        for (String[] c : clothes) {
+            String type = c[1]; 
+            map.put(type, map.getOrDefault(type, 0) + 1);
         }
-        
-        // 조합의 수 계산
+
+        // 조합의 수를 계산하기 위한 변수 초기화
         int answer = 1;
-        for (int count : clothesMap.values()) {
+
+        // 각 의상 종류마다 곱함
+        for (int count : map.values()) {
             answer *= (count + 1);
         }
-        
-        // 아무것도 입지 않는 경우 제외
+
+        // 아무것도 입지 않는 경우를 제외
         return answer - 1;
     }
 }
