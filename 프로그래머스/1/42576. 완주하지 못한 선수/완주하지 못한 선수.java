6977795136624503580e42e190d1a0e7c341b0c5
choijh0309 <128604591+choijh0309@users.noreply.github.com>
2024-09-19 15:57:02 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 79.04 ms, Memory: 111 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,17 +1,23 @@
-import java.util.Arrays;
+import java.util.HashMap;
 
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        Arrays.sort(participant);
-        Arrays.sort(completion);
+        HashMap<String, Integer> map = new HashMap<>();
         
-        int i;
-        for (i = 0; i < completion.length; i++) {
-            if (!participant[i].equals(completion[i])) {
-                return participant[i];
+        for (String p : participant) {
+            map.put(p, map.getOrDefault(p, 0) + 1);
+        }
+        
+        for (String c : completion) {
+            map.put(c, map.get(c) - 1);
+        }
+        
+        for (String key : map.keySet()) {
+            if (map.get(key) != 0) {
+                return key;
             }
         }
         
-        return participant[i];
+        return "";
     }
 }
