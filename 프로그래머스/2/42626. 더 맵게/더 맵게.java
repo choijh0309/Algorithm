261ca0da4a97ebd16a37a6b674e7accd6bef42f6
choijh0309 <128604591+choijh0309@users.noreply.github.com>
2024-09-24 08:49:54 +0900

    [level 2] Title: 더 맵게, Time: 1575.85 ms, Memory: 123 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.java
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -2,30 +2,31 @@ import java.util.PriorityQueue;
 
 class Solution {
     public int solution(int[] scoville, int K) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
-        
-        // 우선순위 큐에 모든 스코빌 지수 추가
-        for (int scov : scoville) {
-            pq.offer(scov);
+        PriorityQueue<Integer> heap = new PriorityQueue<>();
+
+        // 모든 음식의 스코빌 지수를 힙에 추가
+        for (int s : scoville) {
+            heap.offer(s);
         }
-        
-        int mixCount = 0;
-        
-        while (pq.size() > 1 && pq.peek() < K) {
-            int least = pq.poll();
-            int secondLeast = pq.poll();
-            
-            int newScov = least + (secondLeast * 2);
-            pq.offer(newScov);
-            
-            mixCount++;
+
+        int mixCount = 0; 
+
+        // 힙의 최소값이 K 이상이 될 때까지 반복
+        while (heap.size() >= 2 && heap.peek() < K) {
+            int leastSpicy = heap.poll(); 
+            int secondLeastSpicy = heap.poll(); 
+
+            int mixedScoville = leastSpicy + (secondLeastSpicy * 2); 
+
+            heap.offer(mixedScoville); 
+            mixCount++; 
         }
-        
-        // 모든 음식의 스코빌 지수를 K 이상으로 만들 수 없는 경우
-        if (pq.peek() < K) {
-            return -1;
+
+        // 모든 음식의 스코빌 지수가 K 이상인지 확인
+        if (heap.peek() != null && heap.peek() >= K) {
+            return mixCount; 
+        } else {
+            return -1; 
         }
-        
-        return mixCount;
     }
 }
