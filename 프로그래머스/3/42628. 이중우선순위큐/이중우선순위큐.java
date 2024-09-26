714ea038b22650bcb68d69c9425c7d875b12e6d2
choijh0309 <128604591+choijh0309@users.noreply.github.com>
2024-09-26 09:06:35 +0900

    [level 3] Title: 이중우선순위큐, Time: 76.59 ms, Memory: 132 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
@@ -2,56 +2,79 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String[] operations) {
+        // 최소 힙 (오름차순 정렬)
         PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        // 최대 힙 (내림차순 정렬)
         PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
-        Map<Integer, Integer> numberCount = new HashMap<>();
+        Map<Integer, Integer> countMap = new HashMap<>();
+
+        int size = 0; 
 
         for (String operation : operations) {
             String[] parts = operation.split(" ");
-            String command = parts[0];
-            int number = Integer.parseInt(parts[1]);
+            String command = parts[0]; 
+            int number = 0;
+            if (parts.length > 1) {
+                number = Integer.parseInt(parts[1]);
+            }
 
             if (command.equals("I")) {
                 minHeap.offer(number);
-                maxHeap.offer(number);
-                numberCount.put(number, numberCount.getOrDefault(number, 0) + 1);
+                maxHeap.offer(number); 
+                countMap.put(number, countMap.getOrDefault(number, 0) + 1);
+                size++; 
             } else if (command.equals("D")) {
+                if (size == 0) {
+                    continue;
+                }
                 if (number == 1) {
-                    removeNumber(maxHeap, numberCount);
+                    while (!maxHeap.isEmpty()) {
+                        int max = maxHeap.poll(); 
+                        int cnt = countMap.getOrDefault(max, 0);
+                        if (cnt > 0) {
+                            countMap.put(max, cnt - 1);
+                            size--; 
+                            break;
+                        }
+                    }
                 } else if (number == -1) {
-                    removeNumber(minHeap, numberCount);
+                    while (!minHeap.isEmpty()) {
+                        int min = minHeap.poll(); 
+                        int cnt = countMap.getOrDefault(min, 0);
+                        if (cnt > 0) {
+                            countMap.put(min, cnt - 1);
+                            size--; 
+                            break;
+                        }
+                    }
                 }
             }
         }
 
-        // 최종 결과 계산
-        int max = getNumber(maxHeap, numberCount);
-        int min = getNumber(minHeap, numberCount);
-
-        return new int[]{max, min};
-    }
-
-    private void removeNumber(PriorityQueue<Integer> heap, Map<Integer, Integer> numberCount) {
-        while (!heap.isEmpty()) {
-            int number = heap.poll();
-            if (numberCount.containsKey(number)) {
-                if (numberCount.get(number) == 1) {
-                    numberCount.remove(number);
-                } else {
-                    numberCount.put(number, numberCount.get(number) - 1);
+        if (size == 0) {
+            return new int[]{0, 0};
+        } else {
+            int max = 0;
+            while (!maxHeap.isEmpty()) {
+                int m = maxHeap.poll();
+                int cnt = countMap.getOrDefault(m, 0);
+                if (cnt > 0) {
+                    max = m;
+                    break;
                 }
-                break;
             }
-        }
-    }
 
-    private int getNumber(PriorityQueue<Integer> heap, Map<Integer, Integer> numberCount) {
-        while (!heap.isEmpty()) {
-            int number = heap.poll();
-            if (numberCount.containsKey(number)) {
-                return number;
+            int min = 0;
+            while (!minHeap.isEmpty()) {
+                int m = minHeap.poll();
+                int cnt = countMap.getOrDefault(m, 0);
+                if (cnt > 0) {
+                    min = m;
+                    break;
+                }
             }
+
+            return new int[]{max, min};
         }
-        return 0;
     }
 }
