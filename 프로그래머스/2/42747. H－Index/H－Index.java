f71930bde138077232174d7c65cf22576d2fb278
choijh0309 <128604591+choijh0309@users.noreply.github.com>
2024-09-30 20:58:33 +0900

    [level 2] Title: H-Index, Time: 5.18 ms, Memory: 78.3 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -1,16 +1,24 @@
 import java.util.Arrays;
+import java.util.Collections;
 
 class Solution {
-    private boolean isValid(int [] citations, int h) {
-        int index = citations.length - h;
-        return citations[index] >= h;
-    } 
-    
     public int solution(int[] citations) {
-        Arrays.sort(citations);
-        for (int h = citations.length; h >= 1; h--) {
-            if (isValid(citations, h)) return h;
+        // 배열을 내림차순으로 정렬
+        Integer[] sortedCitations = Arrays.stream(citations)
+                                          .boxed()
+                                          .toArray(Integer[]::new);
+        Arrays.sort(sortedCitations, Collections.reverseOrder());
+
+        int hIndex = 0;
+
+        for (int i = 0; i < sortedCitations.length; i++) {
+            if (sortedCitations[i] >= i + 1) {
+                hIndex = i + 1;
+            } else {
+                break; 
+            }
         }
-        return 0;
+
+        return hIndex;
     }
 }
