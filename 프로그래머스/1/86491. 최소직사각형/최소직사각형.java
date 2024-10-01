eaf0903c22771d0190e5fbe1380d85524e57e82a
choijh0309 <128604591+choijh0309@users.noreply.github.com>
2024-10-01 09:07:15 +0900

    [level 1] Title: 최소직사각형, Time: 0.90 ms, Memory: 86.7 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
@@ -1,16 +1,29 @@
 class Solution {
     public int solution(int[][] sizes) {
-        int maxWidth = 0;
-        int maxHeight = 0;
-        
-        for (int[] size : sizes) {
-            int width = Math.max(size[0], size[1]);
-            int height = Math.min(size[0], size[1]);
-            
-            maxWidth = Math.max(maxWidth, width);
-            maxHeight = Math.max(maxHeight, height);
+        int maxWidth = 0;  
+        int maxHeight = 0; 
+
+        for (int i = 0; i < sizes.length; i++) {
+            int width = sizes[i][0];  
+            int height = sizes[i][1]; 
+
+            // 명함을 회전하여 가로 길이가 세로 길이보다 크도록 조정
+            if (width < height) {
+                // 가로와 세로를 교환
+                int temp = width;
+                width = height;
+                height = temp;
+            }
+
+            if (width > maxWidth) {
+                maxWidth = width;
+            }
+
+            if (height > maxHeight) {
+                maxHeight = height;
+            }
         }
-        
+
         return maxWidth * maxHeight;
     }
 }
