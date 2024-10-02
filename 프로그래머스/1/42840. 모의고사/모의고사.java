379784d0e65b90a68b0737b0cb6ac89db82f28b0
choijh0309 <128604591+choijh0309@users.noreply.github.com>
2024-10-02 09:15:47 +0900

    [level 1] Title: 모의고사, Time: 2.31 ms, Memory: 72 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,37 +1,45 @@
-import java.util.stream.IntStream;
+import java.util.ArrayList;
 
 class Solution {
-    private static final int[][] RULES = { {1, 2, 3, 4, 5}, 
-                                          {2, 1, 2, 3, 2, 4, 2, 5}, 
-                                          {3, 3, 1, 1, 2, 2, 4, 4, 5, 5}};
-    
-    private int getPicked(int person, int problem) {
-        int[] rule = RULES[person];
-        int index = problem % rule.length;
-        return rule[index];
-    }
-    
     public int[] solution(int[] answers) {
-        int[] corrects = new int[3];
-        int max = 0;
-        
-        for (int problem = 0; problem < answers.length; problem++) {
-            int answer = answers[problem];
-            
-            for (int person = 0; person < 3; person++) {
-                int picked = getPicked(person, problem);
-                if (answer == picked) {
-                    if (++corrects[person] > max) {
-                        max = corrects[person];
-                    }
-                }
+        int[] firstPattern = {1, 2, 3, 4, 5}; 
+        int[] secondPattern = {2, 1, 2, 3, 2, 4, 2, 5}; 
+        int[] thirdPattern = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5}; 
+
+        int[] scores = {0, 0, 0}; 
+
+        // 문제를 순회하며 각 수포자의 답안과 비교
+        for (int i = 0; i < answers.length; i++) {
+            if (answers[i] == firstPattern[i % firstPattern.length]) {
+                scores[0]++;
+            }
+            if (answers[i] == secondPattern[i % secondPattern.length]) {
+                scores[1]++;
+            }
+            if (answers[i] == thirdPattern[i % thirdPattern.length]) {
+                scores[2]++; 
             }
         }
-        
-        final int maxCorrects = max;
-        return IntStream.range(0, 3)
-                        .filter(i -> corrects[i] == maxCorrects)
-                        .map(i -> i + 1)
-                        .toArray();
+
+        int maxScore = Math.max(scores[0], Math.max(scores[1], scores[2]));
+
+        // 최고 점수를 받은 수포자를 저장할 리스트 생성
+        ArrayList<Integer> list = new ArrayList<>();
+        if (scores[0] == maxScore) {
+            list.add(1); 
+        }
+        if (scores[1] == maxScore) {
+            list.add(2); 
+        }
+        if (scores[2] == maxScore) {
+            list.add(3); 
+        }
+
+        int[] answer = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            answer[i] = list.get(i);
+        }
+
+        return answer;
     }
 }
