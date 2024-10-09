75d7bd162ac9c09415668556d084f50263479680
choijh0309 <128604591+choijh0309@users.noreply.github.com>
2024-10-09 11:38:34 +0900

    [level 2] Title: 피로도, Time: 2.80 ms, Memory: 75.5 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,20 +1,27 @@
 class Solution {
-    private int maxDungeons = 0;
-    
+    int maxCount = 0; 
+    boolean[] visited; 
+
     public int solution(int k, int[][] dungeons) {
-        boolean[] visited = new boolean[dungeons.length];
-        dfs(k, dungeons, visited, 0);
-        return maxDungeons;
+        int n = dungeons.length;
+        visited = new boolean[n]; 
+
+        dfs(k, dungeons, 0);
+
+        return maxCount; 
     }
-    
-    private void dfs(int fatigue, int[][] dungeons, boolean[] visited, int count) {
-        maxDungeons = Math.max(maxDungeons, count);
-        
+
+    private void dfs(int fatigue, int[][] dungeons, int count) {
+        if (count > maxCount) {
+            maxCount = count;
+        }
+
+        // 모든 던전에 대해 시도
         for (int i = 0; i < dungeons.length; i++) {
             if (!visited[i] && fatigue >= dungeons[i][0]) {
-                visited[i] = true;
-                dfs(fatigue - dungeons[i][1], dungeons, visited, count + 1);
-                visited[i] = false;  // 백트래킹
+                visited[i] = true; 
+                dfs(fatigue - dungeons[i][1], dungeons, count + 1);
+                visited[i] = false; 
             }
         }
     }
