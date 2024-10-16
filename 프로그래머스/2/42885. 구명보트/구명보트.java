3094e87714eab3b8d398441d82dd77ed4cc96a7b
choijh0309 <128604591+choijh0309@users.noreply.github.com>
2024-10-17 08:19:41 +0900

    [level 2] Title: 구명보트, Time: 11.43 ms, Memory: 56.1 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -2,23 +2,24 @@ import java.util.Arrays;
 
 class Solution {
     public int solution(int[] people, int limit) {
-        Arrays.sort(people);  // 몸무게를 오름차순으로 정렬
-        int boats = 0;
-        int left = 0;
-        int right = people.length - 1;
-        
+        // 사람들의 몸무게를 오름차순으로 정렬
+        Arrays.sort(people);
+
+        int answer = 0; // 필요한 구명보트의 최소 개수
+        int left = 0; // 가장 가벼운 사람의 인덱스
+        int right = people.length - 1; // 가장 무거운 사람의 인덱스
+
         while (left <= right) {
+            // 가장 가벼운 사람과 가장 무거운 사람의 합이 제한 이하인지 확인
             if (people[left] + people[right] <= limit) {
-                // 가장 가벼운 사람과 가장 무거운 사람을 함께 태움
-                left++;
-                right--;
-            } else {
-                // 가장 무거운 사람만 태움
-                right--;
+                left++; // 둘이 함께 탈 수 있으므로 가벼운 사람의 인덱스를 증가
             }
-            boats++;
+            // 가장 무거운 사람은 보트에 탑승하므로 인덱스를 감소
+            right--;
+            // 보트의 개수 증가
+            answer++;
         }
-        
-        return boats;
+
+        return answer;
     }
 }
