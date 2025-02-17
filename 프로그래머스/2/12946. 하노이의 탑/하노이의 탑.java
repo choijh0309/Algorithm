7bacde10dff3704e77eaea06d481be927f1f3012
choijh0309 <128604591+choijh0309@users.noreply.github.com>
2025-02-17 18:07:22 +0900

    [level 2] Title: 하노이의 탑, Time: 1.94 ms, Memory: 107 MB -BaekjoonHub

--- 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.java
+++ 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-class Solution {
+public class Solution {
     private void hanoi(int n, int from, int to, List<int[]> process) {
         if (n == 1) {
             process.add(new int[] {from, to});
@@ -17,6 +17,6 @@ class Solution {
     public int[][] solution(int n) {
         List<int[]> process = new ArrayList<>();
         hanoi(n, 1, 3, process);
-        return process.toArray(new int[0][1]);
+        return process.toArray(new int[0][]);
     }
 }
