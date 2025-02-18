171db74a0d87099c4c70baf09da746b24c63b98e
choijh0309 <128604591+choijh0309@users.noreply.github.com>
2025-02-18 09:57:17 +0900

    [level 2] Title: 카펫, Time: 9.49 ms, Memory: 78.2 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,22 +1,16 @@
-class Solution {
+public class Solution {
     public int[] solution(int brown, int yellow) {
-        int total = brown + yellow;
-
-        for (int h = 3; h <= total / h; h++) {
-            if (total % h == 0) {
-                int w = total / h; 
-
-                if (w >= h) {
-                    int yellowArea = (w - 2) * (h - 2); 
-
-                    // 노란색 영역이 주어진 yellow와 같으면 정답 반환
-                    if (yellowArea == yellow) {
-                        return new int[]{w, h};
-                    }
+        for (int width = 3; width <= 5000; width++) {
+            for (int height = 3; height <= width; height++) {
+                int boundary = (width + height - 2) * 2;
+                int center = width * height - boundary;
+                
+                if (brown == boundary && yellow == center) {
+                    return new int[] {width, height};
                 }
             }
         }
-
+        
         return null;
     }
 }
