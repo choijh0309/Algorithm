50f815c0130febfa9ef8dd0f9c06686484f4890f
choijh0309 <128604591+choijh0309@users.noreply.github.com>
2025-02-18 10:22:22 +0900

    [level 2] Title: 소수 찾기, Time: 5.30 ms, Memory: 89.4 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -1,58 +1,31 @@
 import java.util.*;
 
-class Solution {
-    static int MAX = 10000000;
-    static boolean[] isPrime = new boolean[MAX];
-    static Set<Integer> numberSet = new HashSet<>();
-
-    public int solution(String numbers) {
-        Arrays.fill(isPrime, true);
-        sieveOfEratosthenes();
-
-        char[] digits = numbers.toCharArray();
+public class Solution {
+    private boolean isPrime(int n) {
+        if (n <= 1) return false;
+        for (int i = 2; i * i <= n; i++) {
+            if (n % i == 0) return false;
+        }
+        return true;
+    }
+    
+    private void getPrimes(int acc, int[] numbers, boolean[] isUsed, Set<Integer> primes) {
+        if (isPrime(acc)) primes.add(acc);
         
-        boolean[] visited = new boolean[digits.length];
-
-        generateNumbers("", digits, visited);
-
-        int count = 0; 
-
-        for (int num : numberSet) {
-            if (isPrime[num] && num > 1) {
-                count++;
-            }
-        }
-
-        return count;
-    }
-
-    private void sieveOfEratosthenes() {
-        isPrime[0] = false;
-        isPrime[1] = false;
-
-        for (int i = 2; i * i < MAX; i++) {
-            if (isPrime[i]) {
-                for (int j = i * i; j < MAX; j += i) {
-                    isPrime[j] = false;
-                }
-            }
+        for (int i = 0; i < numbers.length; i++) {
+            if (isUsed[i]) continue;
+            int nextAcc = acc * 10 + numbers[i];
+            
+            isUsed[i] = true;
+            getPrimes(nextAcc, numbers, isUsed, primes);
+            isUsed[i] = false;
         }
     }
-
-    // 가능한 모든 숫자 조합을 생성하는 함수
-    private void generateNumbers(String current, char[] digits, boolean[] visited) {
-        if (!current.equals("")) {
-            int num = Integer.parseInt(current);
-            numberSet.add(num); 
-        }
-
-        // 모든 자리의 숫자를 시도
-        for (int i = 0; i < digits.length; i++) {
-            if (!visited[i]) {
-                visited[i] = true; 
-                generateNumbers(current + digits[i], digits, visited); 
-                visited[i] = false; 
-            }
-        }
+    
+    public int solution(String nums) {
+        Set<Integer> primes = new HashSet<>();
+        int[] numbers = nums.chars().map(c -> c - '0').toArray();
+        getPrimes(0, numbers, new boolean[numbers.length], primes);
+        return primes.size();
     }
 }
