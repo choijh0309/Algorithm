1a2ce97787392b7e2de0a9edcd5138fb31295469
choijh0309 <128604591+choijh0309@users.noreply.github.com>
2025-02-20 16:51:32 +0900

    [level 0] Title: A로 B 만들기, Time: 1.52 ms, Memory: 94.7 MB -BaekjoonHub

--- 프로그래머스/0/120886. A로 B 만들기/A로 B 만들기.java
+++ 프로그래머스/0/120886. A로 B 만들기/A로 B 만들기.java
@@ -1,6 +1,7 @@
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
 
-class Solution {
+public class Solution {
     private static Map<Character, Integer> toMap(String word) {
         Map<Character, Integer> map = new HashMap<>();
         for (char c : word.toCharArray()) {
@@ -9,7 +10,6 @@ class Solution {
         }
         return map;
     }
-    
     public int solution(String before, String after) {
         return toMap(before).equals(toMap(after)) ? 1 : 0;
     }
