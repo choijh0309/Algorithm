47683f72d75ae04f28e2da5e2d0f3f9ffaffb808
choijh0309 <128604591+choijh0309@users.noreply.github.com>
2025-02-20 16:32:13 +0900

    [level 3] Title: 징검다리 건너기, Time: 41.18 ms, Memory: 83.1 MB -BaekjoonHub

--- 프로그래머스/3/64062. 징검다리 건너기/README.md
+++ 프로그래머스/3/64062. 징검다리 건너기/README.md
@@ -1,10 +1,10 @@
 # [level 3] 징검다리 건너기 - 64062 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/64062?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/64062) 
 
 ### 성능 요약
 
-메모리: 61.3 MB, 시간: 22.35 ms
+메모리: 83.1 MB, 시간: 41.18 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 15일 18:47:19
+2025년 02월 20일 16:32:11
 
 ### 문제 설명
 
--- 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.java
+++ 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.java
@@ -1,33 +1,32 @@
+import java.util.*;
+
 class Solution {
     public int solution(int[] stones, int k) {
-        int left = 1;
-        int right = 200000000;
-        
+        int left = 1;  // 최소 1명
+        int right = Arrays.stream(stones).max().getAsInt();  // 최대 stones 중 가장 큰 값
+
         while (left <= right) {
-            int mid = left + (right - left) / 2;
+            int mid = (left + right) / 2;
             if (canCross(stones, k, mid)) {
-                left = mid + 1;
+                left = mid + 1;  // 더 많은 사람이 건널 수 있는지 확인
             } else {
-                right = mid - 1;
+                right = mid - 1; // 너무 많으므로 줄이기
             }
         }
-        
+
         return right;
     }
-    
+
     private boolean canCross(int[] stones, int k, int people) {
-        int consecutiveZeros = 0;
+        int skip = 0;  // 연속된 0 이하의 디딤돌 개수
         for (int stone : stones) {
-            if (stone - people < 0) {
-                consecutiveZeros++;
+            if (stone < people) {
+                skip++;
+                if (skip >= k) return false;  // 건널 수 없음
             } else {
-                consecutiveZeros = 0;
-            }
-            
-            if (consecutiveZeros >= k) {
-                return false;
+                skip = 0;  // 초기화
             }
         }
-        return true;
+        return true;  // 건널 수 있음
     }
 }
