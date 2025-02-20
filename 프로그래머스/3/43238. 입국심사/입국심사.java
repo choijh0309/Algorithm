98781d6e9324f7fe1506027ed4c5faf6aeb4b855
choijh0309 <128604591+choijh0309@users.noreply.github.com>
2025-02-20 16:21:35 +0900

    [level 3] Title: 입국심사, Time: 72.12 ms, Memory: 107 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.java
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -1,4 +1,4 @@
-class Solution {
+public class Solution {
     private boolean isValid(long t, int n, int[] times) {
         long c = 0;
         for (int time : times) {
@@ -6,6 +6,7 @@ class Solution {
         }
         return c >= n;
     }
+    
     public long solution(int n, int[] times) {
         long start = 1;
         long end = 1000000000000000000L;
