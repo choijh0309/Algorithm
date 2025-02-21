5e51f5c31dfe969b3cc47e76ca5f2e4e62341b61
choijh0309 <128604591+choijh0309@users.noreply.github.com>
2025-02-21 16:43:18 +0900

    [level 2] Title: 피보나치 수, Time: 18.98 ms, Memory: 92.4 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
@@ -1,12 +1,12 @@
-import java.util.*;
+import java.util.Arrays;
 
-class Solution {
+public class Solution {
     private final int[] mem = new int[100001];
     private int fibonacci(int n) {
         if (mem[n] != -1) return mem[n];
         if (n == 0 || n == 1) return n;
         
-        return mem[n] = (fibonacci(n - 1) + fibonacci(n - 2)) % 1234567; 
+        return mem[n] = (fibonacci(n - 1) + fibonacci(n - 2)) % 1234567;
     }
     
     public int solution(int n) {
