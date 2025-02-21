d601674fa2c3de92d36de49d37d97b2881eeda5c
choijh0309 <128604591+choijh0309@users.noreply.github.com>
2025-02-21 17:11:25 +0900

    [level 2] Title: 주식가격, Time: 44.91 ms, Memory: 79.1 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -1,24 +1,23 @@
-import java.util.*;
+import java.util.Stack;
 
-class Solution {
+public class Solution {
     public int[] solution(int[] prices) {
-        int n = prices.length;
-        int[] answer = new int[n];
+        int[] answer = new int[prices.length];
+        
         Stack<Integer> stack = new Stack<>();
-
-        for (int i = 0; i < n; i++) {
-            while (!stack.isEmpty() && prices[i] < prices[stack.peek()]) {
-                int idx = stack.pop(); 
-                answer[idx] = i - idx;
+        for (int i = 0; i < prices.length; i++) {
+            while (!stack.isEmpty() && prices[stack.peek()] > prices[i]) {
+                int index = stack.pop();
+                answer[index] = i - index;
             }
-            stack.push(i); 
+            
+            stack.push(i);
         }
-
+        
         while (!stack.isEmpty()) {
-            int idx = stack.pop();
-            answer[idx] = n - idx - 1; // 총 기간에서 현재 인덱스를 빼서 남은 기간 계산
+            int index = stack.pop();
+            answer[index] = prices.length - index - 1;
         }
-
         return answer;
     }
 }
