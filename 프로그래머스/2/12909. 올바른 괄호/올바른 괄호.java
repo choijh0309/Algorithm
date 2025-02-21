4bff6a7ece11743bfd72003e81055cd152224d24
choijh0309 <128604591+choijh0309@users.noreply.github.com>
2025-02-21 17:02:32 +0900

    [level 2] Title: 올바른 괄호, Time: 6.26 ms, Memory: 54.6 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,23 +1,16 @@
-import java.util.Stack;
-
-class Solution {
-    boolean solution(String s) {
-        Stack<Character> stack = new Stack<>();
-
-        for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-
-            if (c == '(') {
-                stack.push(c);
-            } else {
-                if (stack.isEmpty()) {
-                    return false;
-                } else {
-                    stack.pop();
-                }
+public class Solution {
+    public boolean solution(String s) {
+        int counter = 0;
+        
+        for (char c : s.toCharArray()) {
+            switch (c) {
+                    case '(' -> counter++;
+                    case ')' -> {
+                        if (counter-- == 0) return false;
+                    }
             }
         }
         
-        return stack.isEmpty();
+        return counter == 0;
     }
 }
