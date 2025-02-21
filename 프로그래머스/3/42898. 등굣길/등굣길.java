c17ee61da27325db3d8b7a784fda0c2349c2bedd
choijh0309 <128604591+choijh0309@users.noreply.github.com>
2025-02-21 16:51:59 +0900

    [level 3] Title: 등굣길, Time: 1.37 ms, Memory: 53.8 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.java
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -1,6 +1,6 @@
 import java.util.Arrays;
 
-class Solution {
+public class Solution {
     private final int[][] mem = new int[101][101];
     
     private int count(int x, int y, int w, int h, boolean[][] isPuddle) {
@@ -9,10 +9,11 @@ class Solution {
         
         if (mem[x][y] != -1) return mem[x][y];
         if (x == w && y == h) return 1;
+        
         int total = count(x + 1, y, w, h, isPuddle) + count(x, y + 1, w, h, isPuddle);
         return mem[x][y] = total % 1000000007;
     }
-        
+    
     public int solution(int m, int n, int[][] puddles) {
         for (int[] row : mem) {
             Arrays.fill(row, -1);
@@ -21,7 +22,7 @@ class Solution {
         boolean[][] isPuddle = new boolean[n + 1][m + 1];
         for (int[] p : puddles) {
             isPuddle[p[1]][p[0]] = true;
-        } 
+        }
         
         return count(1, 1, m, n, isPuddle);
     }
